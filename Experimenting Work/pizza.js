74ef21991a2d3ad09d5a5c1fe62a4d83d37cf623
jm214 <jm214@hw.ac.uk>
2019-10-15 21:19:20 +0100

    javascript file linked to the html file and the needed variables are added to the javascript

--- Experimenting Work/pizza.js
+++ Experimenting Work/pizza.js
@@ -1,2 +1,9 @@
-function movement() {}
+function movement() {
+var UpMove = false;
+var RightMove = false;
+var DownMove = false;
+var LeftMove = false;
+var xStartCoord = 200;
+var yStartCoord = 200;
+}
     
