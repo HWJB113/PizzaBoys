751174fe4a9845190c89e4aac654567251237268
jm214 <jm214@hw.ac.uk>
2019-10-16 17:20:50 +0100

    Javascript keypresses complete

--- Experimenting Work/pizza.js
+++ Experimenting Work/pizza.js
@@ -5,5 +5,46 @@ var DownMove = false;
 var LeftMove = false;
 var xStartCoord = 200;
 var yStartCoord = 200;
+
+document.addEventListerner('keydown', BeginMovement)
+
+function beginMovement(x) {
+    if (x.keyCode === 38) { 
+        MoveUp = true;
+    }
+        if (x.keycode === 39)  {
+            MoveRight = true;
+        }
+
+        if (x.keycode === 40)  {
+            MoveDown = true;
+        }
+
+        if (x.keycode === 37)  {
+            MoveLeft = true;
+        }
+
+    }
+
+ document.addEventListerner('keyup', releasekey)
+
+ function beginMovement(x) {
+    if (x.keyCode === 38) { 
+         MoveUp = false;
+    }
+        if (x.keycode === 39)  {
+            MoveRight = false;
+        }
+    
+         if (x.keycode === 40)  {
+             MoveDown = false;
+        }
+    
+         if (x.keycode === 37)  {
+             MoveLeft = false;
+         }
+    
+    }
 }
+
     
