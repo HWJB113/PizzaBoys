9abb122dd7259b7411ffd18fef62a9d11b89cbf6
Joseph Morwood <jm214@hw.ac.uk>
2019-11-05 19:06:54 +0000

    files integreated

--- Experimenting Work/pizza.js
+++ Experimenting Work/pizza.js
@@ -135,6 +135,77 @@ function ChangeDirection(NewDirection){
     }
 }
 window.requestAnimationFrame(Movementloop)
+
+$(function(){
+
+
+    var canvas=document.getElementById("gamemap");
+    var ctx=canvas.getContext("2d");
+    var img = document.getElementById("Test.png");
+    
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    
+    var spawnRate=50;
+
+    var spawnRateOfDescent=0.50;
+
+    var lastSpawn=-1;
+
+    var objects=[];
+
+    var startTime=Date.now();
+
+    animate();
+
+    function spawnRandomObject(){
+
+        var t = "Test";
+
+        var object={
+
+            type:t, 
+
+            x:Math.random()*(canvas.width-30)+15,
+
+            y:Math.random() * (canvas.height) + 100 ,
+        }
+
+
+        objects.push(object);
+    }
+
+
+
+    function animate(){
+
+        var time=Date.now();
+
+        if(time>(lastSpawn+spawnRate)){
+            lastSpawn=time;
+            spawnRandomObject();
+        }
+
+
+        requestAnimationFrame(animate);
+
+
+
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
+
+        for(var i=0;i<objects.length;i++){
+            var object=objects[i];
+            ctx.beginPath();
+            ctx.arc(object.x,object.y,8,0,Math.PI*2);
+            ctx.closePath();
+            ctx.fillStyle=object.type;
+            ctx.fill();
+        }
+
+    }
+    ctx.drawImage(img, 10, 10);
+});
 }
 
 
