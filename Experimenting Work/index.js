4b3f75af42c16d55cda6143747ac61fb2560d62e
Joseph Morwood <jm214@hw.ac.uk>
2019-11-23 22:19:25 +0000

    started work on spliting files

--- Experimenting Work/index.js
+++ Experimenting Work/index.js
@@ -1,7 +1,6 @@
 $(function(){
     var canvas=document.getElementById("canvas");
     var ctx=canvas.getContext("2d");
-    
     ctx.canvas.width  = 3500;
     ctx.canvas.height = 3500;
     
@@ -18,13 +17,8 @@ $(function(){
     var spiderimg = new Image();
     spiderimg.src = "Images/spiderR.gif"
 
-    var MoveUp = false;
-    var MoveDownRight = false;
-    var MoveRight = false;
-    var MoveDown = false;
-    var LastDirection;
-    var NewDirection;
-    var MoveLeft = false;
+   
+    
 
     var music;
     music = new Audio("PizzaTime.ogg");
@@ -37,127 +31,10 @@ $(function(){
 
     makeSpider();
     animate();
-
-    document.addEventListener('keydown', BeginMovement)
-
-    function BeginMovement(x) {
-        music.play();
-        spider = spiderman;
-
-       
-
-
-        if (x.keyCode === 87) { 
-              MoveUp = true;
-              NewDirection = 0;
-              
-        }
-      
-        if (x.keyCode === 68)  {
-            MoveRight = true;
-            NewDirection = 1;
+    movement(spiderman);
+   
     
-            
-        }
-      
-        if (x.keyCode === 83)  {
-            MoveDown = true;
-            NewDirection = 2;
-      
-        }
-      
-        if (x.keyCode === 65)  {
-            MoveLeft = true;
-            NewDirection = 3;
-        }
-      
-        ChangeDirection(NewDirection);
-         
-        }
-
-        document.addEventListener('keyup', releaseKey)
-
-    function releaseKey(x) {
-
-        if (x.keyCode === 87) { 
-            MoveUp = false;
-            LastDirection = 0;
-        }
-          
-        if (x.keyCode === 68)  {
-            MoveRight = false;
-            LastDirection = 1;
-        }
-              
-        if (x.keyCode === 83){ 
-            MoveDown = false; 
-            LastDirection = 2;
-        }
-              
-        if (x.keyCode === 65)  {
-            MoveLeft = false;
-            LastDirection = 3;
-            }
-          
-              
-        }
-
-    function Movementloop() {
-        
-        spider = spiderman;
-            
-        if(MoveUp === true && (spider.y - 5) > 20){
-            spider.y = (spider.y - 5);
-         
-                window.scrollBy(0,-5);
-            }
-        
-        if (MoveRight === true && (spider.x + 5) < 3000){
-                spider.x = (spider.x + 5);
-                window.scrollBy(5,0);}
-        else if(MoveRight === true && (spider.x + 5) < 3420) {
-            spider.x = (spider.x + 5);
-        }
-        
-        if (MoveDown === true && (spider.y + 5) < 3420){
-            spider.y = (spider.y + 5)
-            window.scrollBy(0,5);
-        }
-        
-        if (MoveLeft === true && (spider.x - 5) > 20){
-            spider.x = (spider.x - 5);
-                window.scrollBy(-5,0);
-        }
-                
-        window.requestAnimationFrame(Movementloop)
-                
-        }
-
-    function ChangeDirection(NewDirection){
-
-        spider = spiderman;
-
-        if (NewDirection !== LastDirection ){
-            if (NewDirection === 0){
-                spiderimg.src="Images/spiderU.gif"
-                LastDirection = NewDirection;
-            }
-            if (NewDirection === 1){
-                spiderimg.src="Images/spiderR.gif"
-                LastDirection = NewDirection;
-            }
-            if (NewDirection === 2){
-                spiderimg.src="Images/spiderD.gif"
-                LastDirection = NewDirection;
-            }
-            if (NewDirection === 3){
-                spiderimg.src="Images/spiderL.gif"
-                LastDirection = NewDirection;
-            }
-            }
-        }
-        
-        window.requestAnimationFrame(Movementloop)
+   
 
 
     function spawnObject(){
@@ -187,13 +64,14 @@ $(function(){
             height: 75,
       
             image: spiderimg
-      
+            
             }
-      
             spiderman = spider;
+            
+            
     
     }
-
+    
 
     function animate(){
 
