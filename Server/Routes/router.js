0f34d24b5d255fe516ec8917f0297a3822d65b16
Jake Barr <jb113@hw.ac.uk>
2019-11-24 15:51:21 +0000

    Added more mongoDB support and updated depencdancies

--- Server/Routes/router.js
+++ Server/Routes/router.js
@@ -1,142 +1,41 @@
-const express = require('express');
-const router = express.Router();
-const data_b = require('../Config/database');
-const Player = require('../Models/player_model');
-const Team = require('../Models/team_model');
-const crypt = require('bcryptjs');
-const bodyParser = require('body-parser');
-var app = express();
-
-
-
+//const express = require('express');
+//const router = express.Router();
+//const data_b = require('../Config/database');
+//const Player = require('../Models/player_model');
+//const Team = require('../Models/team_model');
+//const crypt = require('bcryptjs');
+//const bodyParser = require('body-parser');
+//var app = express();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://PizzaBoys:PizzaPizza@pizzaboys-lbq8c.azure.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  db.mycol.insert({
-   _id: ObjectId(7df78ad8902c),
-   Username: 'MongoDB Overview', 
-   Pasword: 'MongoDB is no sql database',
-   CurrentScore: 600,
-   HighScore: 1000
-   Team: 2
-})
-  client.close();
-});
 
-
-
-
-
-app.use(session({
-	secret: 'pizzatime',
-	resave: true,
-	saveUninitialized: true
-}));
-
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-
-app.get('/', function(req, res){
-	response.sendFile(path.join(__dirname + '/sign_in.html'));
-});
-
-function teamAssign() {
-    team = Math.floor(Math.random() * 2) + 1;
-    return team;
+function sign_up(){
+    MongoClient.connect(uri, function(err, db){
+        if (err) throw err;
+        var dbo = db.db("PizzaBoys");
+        var myobj = {Username: "Tobey_Maguire" , Password: "Tobey", CurrentScore: 0, HighScore: 0, Team: 1}
+        dbo.collection("Players").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("Player added");
+            db.close()
+        });
+    });
 
 }
 
-app.post('/signup', (request, response) => {
-    var Username = request.body.Username
-	var Password = request.body.Password
-
-    crypt.genSalt(7, function(error, salt) {
-        crypt.hash(Password, salt, function(error, hashed) {
-            if (error) {
-                res.status(500).send("Internal error")
-            }
+function sign_in(){
+    MongoClient.connect(uri, function(err, db) {
+        if(err) throw err;
+        var User = document.getElementById("user").value;
+        var Pass = document.getElementById("pass").value;
+        var dbo = db.db("PizzaBoys");
+        var query = { Username: User, Password: Pass }
+        dbo.collection("Players").find(query).toArray(function(err,result) {
+            if(err) throw alert("Incorrect Username or Password!");
+            
+            db.close();
         })
-
-        Player.create({
-                Username,
-                Password: hashed,
-                CurrentScore: 0,
-                HighScore: 0,
-                Team: teamAssign()
-            })
-            .then(Player => res.send(Player))
-            .catch(err => res.status(500).send(err));
-
-        Team.update({
-                NumOfPlayers: +1
-            }, {
-                where: {
-                    Team
-                }
-            })
-            .then(Team => res.send(Team))
-            .catch(err => res.status(500).send(err));
-
     })
-    alert("FUCKKKKKKKK");
-})
+}
 
-
-
-
-app.post('/login', (request, response) => {
-    var Username = request.body.Username
-	var Password = request.body.Password
-
-	if(Username && Password) {
-		connection.query('SELECT * FROM Player WHERE Username = ? AND Password = ?', [Username, Password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = Username;
-				response.redirect('/game');
-			} else {
-				response.send('Wrong username/password!');
-			}
-			response.end();
-		});
-	} else {
-		response.send('Please enter usename and password');
-		response.end();
-	}
-});
-
-app.get('/game', function(request, response) {
-	if (request.session.loggedin) {
-		re
-	}
-})
-  //  Player.findOne({ where: Username }).then(Player => {
-    //    var hashed = Player.Password;
-      //  crypt.compare(Password, hashed, function(err, hash) {
-       //     if (err || !hash) {
-         //       res.status(500).send("Internal error")
-           // }
-		   //
-           // res.send(Player)
-       // })
-   // })
-//})
-
-
-router.post("/scoreupdate", (req, res) => {
-    const { Username, HighScore } = req.body;
-    Player.update({
-            HighScore
-        }, {
-            where: {
-                Username
-            }
-        })
-        .then(Player => res.send(Player))
-        .catch(err => res.status(500).send(err));
-})
-
-moudle.exports = router;
