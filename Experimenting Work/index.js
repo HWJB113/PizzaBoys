e667c9873cee6aa0f93a3b64e33e37c35755ca1b
Morwood <jm214@hw.ac.uk>
2019-11-24 14:48:24 +0000

    fixed game for the master

--- Experimenting Work/index.js
+++ Experimenting Work/index.js
@@ -1,121 +1,240 @@
-$(function(){
+$(function gamestart(){
     var canvas=document.getElementById("canvas");
     var ctx=canvas.getContext("2d");
+
     ctx.canvas.width  = 3500;
     ctx.canvas.height = 3500;
-    
-    
+    window.scrollTo(600,800)
     var spawnRate=1000;
-
     var lastSpawn=-1;
-
     var objects=[];
-
-    var pizza = new Image();
-    pizza.src = "Images/pizza.png"
-
     var spiderman;
     var spiderimg = new Image();
     spiderimg.src = "Images/spiderR.gif"
+    var pizza = new Image();
+    pizza.src = "Images/pizza.png"
+    var homebaseS = new Image();
+    homebaseS.src = "Images/homeBase"
+
+    var MoveUp = false;
+    var MoveRight = false;
+    var MoveDown = false;
+    var LastDirection;
+    var NewDirection;
+    var MoveLeft = false;
 
 
-    
 
     var music;
     music = new Audio("PizzaTime.ogg");
     music.addEventListener('ended', LoopMusic)
-
     function LoopMusic(){
         music.currentTime = 0;
         music.play();
     }
 
-    var ps_team = 0;
-    var fj_team = 0;
-
-    assignTeam();
-    
     makeSpider();
     animate();
 
 
+    document.addEventListener('keydown', BeginMovement)
 
-    function spawnObject(){
+    function BeginMovement(x) {
+        music.play();
+        spider = spiderman;
 
 
-        var object={
-            x:Math.random()*(canvas.width-30)+15,
 
-            y:Math.random() * (canvas.height) + 100 ,
 
-            width: 30,
+        if (x.keyCode === 87) { 
+              MoveUp = true;
+              NewDirection = 0;
 
-            height: 30,
-
-            image: pizza
         }
 
+        if (x.keyCode === 68)  {
+            MoveRight = true;
+            NewDirection = 1;
+        }
 
+        if (x.keyCode === 83)  {
+            MoveDown = true;
+            NewDirection = 2;
+        }
+
+        if (x.keyCode === 65)  {
+            MoveLeft = true;
+            NewDirection = 3;
+        }
+
+        ChangeDirection(NewDirection);
+
+        }
+
+        document.addEventListener('keyup', releaseKey)
+
+    function releaseKey(x) {
+
+        if (x.keyCode === 87) { 
+            MoveUp = false;
+            LastDirection = 0;
+        }
+
+        if (x.keyCode === 68)  {
+            MoveRight = false;
+            LastDirection = 1;
+        }
+
+        if (x.keyCode === 83){ 
+            MoveDown = false; 
+            LastDirection = 2;
+        }
+
+        if (x.keyCode === 65)  {
+            MoveLeft = false;
+            LastDirection = 3;
+            }
+
+
+        }
+
+    function Movementloop() {
+
+        spider = spiderman;
+
+        if(MoveUp === true && (spider.y - 5) > 20){
+            spider.y = (spider.y - 5);
+
+                window.scrollBy(0,-5);
+            }
+
+        if (MoveRight === true && (spider.x + 5) < 3000){
+                spider.x = (spider.x + 5);
+                window.scrollBy(5,0);}
+        else if(MoveRight === true && (spider.x + 5) < 3420) {
+            spider.x = (spider.x + 5);
+        }
+
+        if (MoveDown === true && (spider.y + 5) < 3420){
+            spider.y = (spider.y + 5)
+            window.scrollBy(0,5);
+        }
+
+        if (MoveLeft === true && (spider.x - 5) > 20){
+            spider.x = (spider.x - 5);
+                window.scrollBy(-5,0);
+        }
+
+        window.requestAnimationFrame(Movementloop)
+
+        }
+
+    function ChangeDirection(NewDirection){
+
+        spider = spiderman;
+
+        if (NewDirection !== LastDirection ){
+            if (NewDirection === 0){
+                spiderimg.src="Images/spiderU.gif"
+                LastDirection = NewDirection;
+            }
+            if (NewDirection === 1){
+                spiderimg.src="Images/spiderR.gif"
+                LastDirection = NewDirection;
+            }
+            if (NewDirection === 2){
+                spiderimg.src="Images/spiderD.gif"
+                LastDirection = NewDirection;
+            }
+            if (NewDirection === 3){
+                spiderimg.src="Images/spiderL.gif"
+                LastDirection = NewDirection;
+            }
+            }
+        }
+
+        window.requestAnimationFrame(Movementloop)
+
+
+
+    function spawnObject(){
+        var object={
+            x:Math.random()*(canvas.width-30)+15,
+            y:Math.random() * (canvas.height) + 100 ,
+            width: 30,
+            height: 30,
+            image: pizza
+        }
         objects.push(object);
     }
     function makeSpider(){
+        var spidyX = Math.random()*(canvas.width-30);
+        var spidyY = Math.random() * (canvas.height);
         var spider={
-            x:600,
+            x:spidyX,
       
-            y:450 ,
+            y:spidyY ,
       
             width: 75,
             height: 75,
-      
 
-            image: spiderimg,
+            image: spiderimg}
+            window.scrollTo(spidyX, spidyY + 300)
+            spiderman = spider;}
 
-            team: assignedTeam,
-      
-=======
-            image: spiderimg
-            
+          //  function home(){
+            //    var homebaseS={
+                //    x:0,
+                //    y:450,
+        
+                 //   width: 100,
+                   // height: 100,
+        
+                  //  image: homebaseS}
+                    
+                  //  HBS = homebaseS;
+                    
+           // }
 
-            }
-            spiderman = spider;
-            
-            
-    
-    }
-    
+           function moveup() {
+            spider.y = (spider.y - 5);
+
+            window.scrollBy(0,-5);
+          }
+          
+          function movedown() {
+            myGamePiece.speedY += 1;
+          }
+          
+          function moveleft() {
+            myGamePiece.speedX -= 1;
+          }
+          
+          function moveright() {
+            myGamePiece.speedX += 1;
+          }
 
     function animate(){
-
         var time=Date.now();
-
         if(time>(lastSpawn+spawnRate)){
             lastSpawn=time;
             spawnObject();
         }
         requestAnimationFrame(animate);
-
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         var spider = spiderman;
+        
         for (var i = 0; i < objects.length; i++) {
             var object = objects[i];
             ctx.drawImage(object.image, object.x, object.y, object.width, object.height);           
             ctx.drawImage(spider.image, spider.x, spider.y, spider.height, spider.width);
-
+          
             if(spider.x < object.x + object.width &&
                 spider.x + spider.width > object.x &&
                 spider.y < object.y + object.height &&
                 spider.y + spider.height > object.y){
                     ctx.clearRect(object.x, object.y, object.width, object.height)
                     objects.splice(i, 1)
-                    if (spider.team == 1) {
-                        team_ps_score += 1;
-                        document.getElementById('ps_score').innerHTML = "" + team_ps_score;
-                    }
-                    else {
-                        team_fj_score += 1;
-                        document.getElementById('fj_score').innerHTML = "" + team_fj_score;
-                    }
-
                 }
                
         }
